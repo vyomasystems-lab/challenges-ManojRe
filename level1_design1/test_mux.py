af1470dc9e5d35c2432ffbc5008d6a07dd98edcb
Manoj <manoj.sreddy@somaiya.edu>
2022-07-22 08:28:08 +0000

    added test_mux

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -13,36 +13,7 @@ async def test_mux1(dut):
     INP13 = 1
     # input driving
     dut.inp13.value = INP13
-    # dut.inp0.value = Not_inp13
-    # dut.inp1.value = Not_inp13
-    # dut.inp2.value = Not_inp13
-    # dut.inp3.value = Not_inp13
-    # dut.inp4.value = Not_inp13
-    # dut.inp5.value = Not_inp13
-    # dut.inp6.value = Not_inp13
-    # dut.inp7.value = Not_inp13
-    # dut.inp8.value = Not_inp13
-    # dut.inp9.value = Not_inp13
-    # dut.inp10.value = Not_inp13
-    # dut.inp11.value = Not_inp13
     dut.inp12.value = Not_inp13
-    # dut.inp14.value = Not_inp13
-    # dut.inp15.value = Not_inp13
-    # dut.inp16.value = Not_inp13
-    # dut.inp17.value = Not_inp13
-    # dut.inp18.value = Not_inp13
-    # dut.inp19.value = Not_inp13
-    # dut.inp20.value = Not_inp13
-    # dut.inp21.value = Not_inp13
-    # dut.inp22.value = Not_inp13
-    # dut.inp23.value = Not_inp13
-    # dut.inp24.value = Not_inp13
-    # dut.inp25.value = Not_inp13
-    # dut.inp26.value = Not_inp13
-    # dut.inp27.value = Not_inp13
-    # dut.inp28.value = Not_inp13
-    # dut.inp29.value = Not_inp13
-    #dut.inp30.value = Not_inp13
     dut.sel.value = SEL
     await Timer(2, units='ns')
     dut._log.info(f'Sel={SEL:05} Inp13={INP13:05} Output={INP13:05} DUT={int(dut.out.value):05}')
