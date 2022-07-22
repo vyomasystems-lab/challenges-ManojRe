b6506c97ac45762017ef076bcb9b8bfb070699d4
Manoj <manoj.sreddy@somaiya.edu>
2022-07-22 07:26:05 +0000

    added mux

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,3 +8,11 @@ async def test_mux(dut):
     """Test for mux2"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
+    SEL = 5'b1101
+    INP13 = 1
+    # input driving
+    dut.inp13.value = INP13
+    dut.sel.value = SEL
+    await Timer(2, units='ns')
+
+    assert dut.sel.value == INP13, f"Multiplexer selection is incorrect: {dut.sel.value} != 1"
